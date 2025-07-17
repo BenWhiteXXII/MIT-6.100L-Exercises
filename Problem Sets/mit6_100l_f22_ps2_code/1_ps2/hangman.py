ba9b7d00d107ab06c55f0a021d01e6456af0464c
BenWhiteXXII <ben.white.201103@gmail.com>
2025-07-17 15:48:11 +0100

    Completed problem set 2

--- Problem Sets/mit6_100l_f22_ps2_code/1_ps2/hangman.py
+++ Problem Sets/mit6_100l_f22_ps2_code/1_ps2/hangman.py
@@ -1,7 +1,7 @@
 # Problem Set 2, hangman.py
 # Name: Ben White
 # Collaborators: N/A
-# Time spent:
+# Time spent: 3 hrs
 
 import random
 import string
@@ -95,12 +95,58 @@ def get_available_letters(letters_guessed):
     # FILL IN YOUR CODE HERE AND DELETE "pass"
     alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     pos = 0
-    for i in alphabet:
-        if i in letters_guessed:
+    for i in range(len(alphabet)):
+        if alphabet[pos] in letters_guessed:
             alphabet.pop(pos)
-        pos += 1
+        else:
+            pos += 1
     return str(''.join(alphabet))
 
+def with_help_helper(secret_word, letters_guessed):
+    for i in secret_word:
+        if i not in letters_guessed:
+            letters_guessed.append(i)
+            print(f"Letter revealed: {i}")
+            return i
+
+def guess_helper(secret_word, letters_guessed, guesses, guess):
+    if guess == "!" and with_help == True:
+        if guesses > 3:
+            guesses -= 3
+            guess = with_help_helper(secret_word, letters_guessed)
+            return (letters_guessed, guesses)
+        else:
+            print("You don't have enough guesses! Guess again.")
+            return (letters_guessed, guesses)
+    elif type(guess) != str or len(guess) > 1 or guess in r" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~":
+        print("Invalid guess! Guess a single letter.")
+        return (letters_guessed, guesses)
+    else:
+        guess = guess.lower()
+        if guess in letters_guessed:
+            print("Oops! You've already guessed that letter!")
+            return (letters_guessed, guesses)
+        else:
+            letters_guessed.append(guess)
+            if guess in secret_word:
+                print("Correct guess!")
+            else:
+                print("Try again!")
+                if guess in "aeiou":
+                    guesses -= 2
+                else:
+                    guesses -= 1
+            return (letters_guessed, guesses)
+
+def score_calculation(guesses, secret_word):
+    unique_cha = []
+    unique_count = 0
+    for i in secret_word:
+        if i not in unique_cha:
+            unique_cha.append(i)
+            unique_count += 1
+    total_score = (guesses + (4 * len(unique_cha)) + (3 * len(secret_word)))
+    return total_score
 
 def hangman(secret_word, with_help):
     """
@@ -142,6 +188,27 @@ def hangman(secret_word, with_help):
     Follows the other limitations detailed in the problem write-up.
     """
     # FILL IN YOUR CODE HERE AND DELETE "pass"
+    guesses = 10
+    letters_guessed = []
+    play = True
+    print(f"The word has {len(secret_word)} characters.")
+    while play == True:
+        print("-------------------------------------")
+        print(f"You have {guesses} guesses remaining.")
+        print(f"Avaliable letters: {get_available_letters(letters_guessed)}")
+        print(get_word_progress(secret_word, letters_guessed))
+        guess = input("Guess a lowercase letter: ")
+        (letters_guessed, guesses) = guess_helper(secret_word, letters_guessed, guesses, guess)
+        if has_player_won(secret_word, letters_guessed) == True:
+            print("You win!")
+            print(f"Your total score for this game is: {score_calculation(guesses, secret_word)}")
+            play = False
+        elif guesses <= 0:
+            print(f"You ran out of guesses! The word was {secret_word}")
+            play = False
+
+
+
 
 
 
@@ -151,10 +218,9 @@ def hangman(secret_word, with_help):
 if __name__ == "__main__":
     # To test your game, uncomment the following three lines.
 
-    # secret_word = choose_word(wordlist)
-    # with_help = False
-    # hangman(secret_word, with_help)
-
+    secret_word = choose_word(wordlist)
+    with_help = True
+    hangman(secret_word, with_help)
     # After you complete with_help functionality, change with_help to True
     # and try entering "!" as a guess!
 
@@ -165,6 +231,6 @@ if __name__ == "__main__":
     # It doesn't matter if the lines above are commented in or not
     # when you submit your pset. However, please run ps2_student_tester.py
     # one more time before submitting to make sure all the tests pass.
-    secret_word = 'apple'
-    letters_guessed = ['e', 'i', 'k', 'p', 'r', 's']
-    print(get_available_letters(letters_guessed))
+    # secret_word = 'apple'
+    # letters_guessed = ['e', 'i', 'k', 'p', 'r', 's']
+    # print(get_available_letters(letters_guessed))
